a8348d108796dcf01b67394b81017a816a8d0d96
Cattani <contato.cattani@gmail.com>
2021-02-28 17:56:05 -0300

    install absinthe

--- mix.exs
+++ mix.exs
@@ -42,7 +42,9 @@ defmodule TheScore.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.4"},
+      {:absinthe_plug, "~> 1.4"}
     ]
   end
 
