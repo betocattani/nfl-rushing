22eaa02a0046e5d52e41a95f2b059fb4fc97913b
Cattani <contato.cattani@gmail.com>
2021-03-01 18:15:13 -0300

    creates middleware to graphql know how to handle with changeset errors

--- lib/the_score_web/resolvers/players.ex
+++ lib/the_score_web/resolvers/players.ex
@@ -6,32 +6,8 @@ defmodule TheScoreWeb.Resolvers.Players do
   end
 
   def create_profile(_, %{input: params}, _) do
-    case Players.create_profile(params) do
-      {:error, changeset} ->
-        {:ok, %{errors: transform_errors(changeset)}}
-      {:ok, player_profile} ->
-        {:ok, %{player_profile: player_profile}}
+    with {:ok, profile} <- Players.create_profile(params) do
+      {:ok, %{player_profile: profile}}
     end
-  end
-
-  defp transform_errors(changeset) do
-    changeset
-    |> Ecto.Changeset.traverse_errors(&format_error/1)
-    |> Enum.map(fn
-      {key, value} ->
-        %{key: key, message: value}
-    end)
-  end
-
-  @spec format_error(Ecto.Changeset.error) :: String.t
-  defp format_error({msg, opts}) do
-    Enum.reduce(opts, msg, fn {key, value}, acc ->
-      String.replace(acc, "%{#{key}}", to_string(value))
-    end)
-  end
-
-  def error_details(changeset) do
-    changeset
-    |> Ecto.Changeset.traverse_errors(fn {msg, _} -> msg end)
   end
 end
--- lib/the_score_web/schema.ex
+++ lib/the_score_web/schema.ex
@@ -2,6 +2,7 @@ defmodule TheScoreWeb.Schema do
   use Absinthe.Schema
 
   alias TheScoreWeb.Resolvers
+  alias TheScoreWeb.Schema.Middleware
 
   import_types __MODULE__.PlayerTypes
 
@@ -19,6 +20,7 @@ defmodule TheScoreWeb.Schema do
     field :create_player_profile, :player_profile_result do
       arg :input, non_null(:player_profile_input)
       resolve &Resolvers.Players.create_profile/3
+      middleware Middleware.ChangesetErrors
     end
   end
 
