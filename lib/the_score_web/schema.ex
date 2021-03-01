7bf8d34568df10d24918e8eccb5d3c81c1e80c6d
Cattani <contato.cattani@gmail.com>
2021-03-01 18:25:58 -0300

    define middleware functions with middleware changeset error

--- lib/the_score_web/schema.ex
+++ lib/the_score_web/schema.ex
@@ -20,7 +20,6 @@ defmodule TheScoreWeb.Schema do
     field :create_player_profile, :player_profile_result do
       arg :input, non_null(:player_profile_input)
       resolve &Resolvers.Players.create_profile/3
-      middleware Middleware.ChangesetErrors
     end
   end
 
@@ -28,5 +27,12 @@ defmodule TheScoreWeb.Schema do
   object :input_error do
     field :key, non_null(:string)
     field :message, non_null(:string)
+  end
+
+  def middleware(middleware, _field, %{identifier: :mutation}) do
+    middleware ++ [Middleware.ChangesetErrors]
+  end
+  def middleware(middleware, _field, _object) do
+    middleware
   end
 end
