c0bebb109b462795e4a9219bbe4730fe73ec09a5
Cattani <contato.cattani@gmail.com>
2021-03-03 14:21:58 -0300

    removee unecessaru required fields

--- lib/the_score/players/profile.ex
+++ lib/the_score/players/profile.ex
@@ -42,22 +42,7 @@ defmodule TheScore.Players.Profile do
         :rushing_yards_each_forty_plus,
         :rushing_fumbles]
       )
-    |> validate_required([
-      :name,
-      :team,
-      :position,
-      :rushing_attempts_per_game_avg,
-      :rushing_attempts,
-      :total_rushing_yards,
-      :rushing_average_yards_per_attempt,
-      :rushing_yards_per_game,
-      :total_rushing_touchdowns,
-      :longest_rush,
-      :rushing_first_downs,
-      :rushing_first_down_percentage_first,
-      :rushing_yards_each_twenty_plus,
-      :rushing_yards_each_forty_plus,
-      :rushing_fumbles])
+    |> validate_required([:name, :team, :position])
     |> unique_constraint(:name)
   end
 end
